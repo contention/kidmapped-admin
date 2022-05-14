c819a49b56b8b9b0b69588ba8097175e53043c7e
Contention <barry@contention.agency>
2022-05-14 14:23:42 +0000

    Work on comments

--- src/components/commentManager.js
+++ src/components/commentManager.js
@@ -5,22 +5,16 @@ import { db } from '../lib/firebase';
 
 const CommentManager = (props) => {
 
-    let commentStatus = 0;
-
-    let draftView = {draftTabClassName: 'is-active', liveTabClassName: '', deletedTabClassName: ''};
-    let liveView = {draftTabClassName: '', liveTabClassName: 'is-active', deletedTabClassName: ''};
-    let deletedView = {draftTabClassName: '', liveTabClassName: '', deletedTabClassName: 'is-active'};
-
-    const [viewStatus, setViewStatus] = useState(draftView);
     const [comments, setComments] = useState([]);
 
 
-    const getComments = async(commentStatus) => {
+
+    const getDraftComments = async(commentStatus) => {
         if (props.placeId === null) {
             return;
         }
 		let commentList = [];
-		const commentsRef = db.collection('places').doc(props.placeId).collection('comments').where('status', '==', commentStatus);
+		const commentsRef = db.collection('places').doc(props.placeId).collection('comments').where('status', '==', 0);
 		const snapshot = await commentsRef.get();
 		if (snapshot.empty) {
 			console.log('No matching comments.');
@@ -40,26 +34,9 @@ const CommentManager = (props) => {
 	}
 
 
-
-    const handleClickDraftButton = () => {
-        setViewStatus(draftView);
-        getComments(0);
-      }
-
-      const handleClickLiveButton = () => {
-        setViewStatus(liveView);
-        getComments(1);
-      }
-
-      const handleClickDeletedButton = () => {
-        setViewStatus(deletedView);
-        getComments(2);
-      }
-
     const handleClickChangeStatusButton = async(status, commentId) => {
         
         let commentDataToSave = {};
-
         commentDataToSave.updatedAt = firebase.firestore.FieldValue.serverTimestamp();
         commentDataToSave.status = status;
 
@@ -68,64 +45,45 @@ const CommentManager = (props) => {
         await db.collection('places').doc(props.placeId).collection('comments').doc(commentId).update(commentDataToSave)
         .then(() => {
             console.log('Saved');
-            getComments(commentStatus);
         })
         .catch(error => {
-                console.log(error);
+            console.log(error);
         });
-                            
-        
-            
-    
-        
     }
 
 
 
     //On mount
 	useEffect(() => {
-		getComments(commentStatus);
+		getDraftComments();
 	}, []);
 
 
-    let commentsUi =    <div class="notification is-warning">
+    let commentsUi =    <div className="notification is-warning">
                             You need to save this place before you can add comments.
                         </div>;
 
     if (props.placeId !== null) {
         commentsUi = <div>
-        <div className="tabs">
-            <ul>
-                <li className={viewStatus.draftTabClassName}>
-                    <a className="" onClick={handleClickDraftButton}>Draft</a>
-                </li>
-                <li className={viewStatus.liveTabClassName}>
-                    <a className="" onClick={handleClickLiveButton}>Live</a>
-                </li>
-                <li className={viewStatus.deletedTabClassName}>
-                    <a className="" onClick={handleClickDeletedButton}>Deleted</a>
-                </li>
-            </ul>
-            </div>
 
             <ul className="commentsList">
             {comments.map(function(comment, i){
                 return (
                     <li className="comment" key={i}>
                         {comment.data.comment}
-                        <div class="field has-addons">
-                            <p class="control">
-                                <button class="button is-small" onClick={() =>  handleClickChangeStatusButton(0, comment.id)}>
+                        <div className="field has-addons">
+                            <p className="control">
+                                <button className="button is-small" onClick={() =>  handleClickChangeStatusButton(0, comment.id)}>
                                     Draft
                                 </button>
                             </p>
-                            <p class="control">
-                                <button class="button is-small" onClick={() =>  handleClickChangeStatusButton(1, comment.id)}>
+                            <p className="control">
+                                <button className="button is-small" onClick={() =>  handleClickChangeStatusButton(1, comment.id)}>
                                     Live
                                 </button>
                             </p>
-                            <p class="control">
-                                <button class="button is-small" onClick={() =>  handleClickChangeStatusButton(2, comment.id)}>
+                            <p className="control">
+                                <button className="button is-small" onClick={() =>  handleClickChangeStatusButton(2, comment.id)}>
                                     Deleted
                                 </button>
                             </p>
